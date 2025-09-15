b6086076c76d63859e85e5e39e3450e5fe68924d
keyaeugene <keyaeugene@gmail.com>
2025-09-15 16:52:02 +0300

    debugging

--- src/app/api/purchases/routes.ts
+++ src/app/api/purchases/routes.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient, RationCard, Item } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
